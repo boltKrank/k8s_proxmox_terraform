012bc3fdc4affa5c6aab0b543126639d17b8f23e
Simon Anderson <simon.anderson11@gmail.com>
2024-03-20 12:13:39 +1100

    adding gateway config

---
+++ k8s_3/gateway/gateway_terraform/vars.tf
@@ -0,0 +1,50 @@
+variable "proxmox_host" {
+    default = "pve"
+}
+variable "template_name" {
+  type = string
+}
+
+variable "pm_api_url" {
+  type = string
+}
+
+variable "pm_api_token_id" {
+  type = string
+}
+
+variable "pm_api_token_secret" {
+  type = string
+}
+
+variable "pm_ip" {
+  type = string
+}
+
+variable "ssh_user" {
+  type = string
+}
+
+variable "ssh_public_key" {
+  type = string
+}
+
+variable "bootstrap_vm_name" {
+  type = string
+}
+
+variable "ssh_user_password" {
+  type = string
+}
+
+variable "gateway_vm_name" {
+  type = string
+}
+
+variable "internal_bridge" {
+  type = string
+}
+
+variable "external_bridge" {
+  type = string
+}
